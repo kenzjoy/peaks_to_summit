da7ec6cce14c02dbf6e50ad5751a0babfefb9ac2
Mackenzie Leng <kenz.joy@outlook.com>
2022-07-09 17:01:56 -0600

    Add four variables and their contents

---
+++ arrays.rb
@@ -0,0 +1,4 @@
+peaks_to_summit ["Vermillion", "Golden Horn", "Fuller", "Pilot Knob"]
+elevations [13894, 13780, 13761, 13738]
+distance_between_peaks [5.7, 0.43, 1.6, 0.77]
+peaks_summited [true, false, false, true]
